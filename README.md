70f836341faaa035af51f73a4e2e5308ceed697b
yyk <yyk@Ubuntu.myguest.virtualbox.org>
2026-01-19 11:20:09 -0500

    Assignment 4 Part 2

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This repository contains assignment starter code for buildroot based assignments for the course Advanced Embedded Software Design, ECEN 5713
 
 It also contains instructions related to modifying your buildroot project to use with supported hardware platforms.  See [this wiki page](https://github.com/cu-ecen-5013/buildroot-assignments-base/wiki/Supported-Hardware) for details.
+
+## Some tips & tricks
+
+Rebuild w/o checking-in
+
+   $ AESD_ASSIGNMENTS_OVERRIDE_SRCDIR=/home/yyk/src/assignment-3-yyk99 make aesd-assignments-rebuild
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO (DONE): Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'dc21ffc59d6089bea196d932858ee256f26bfe75'
+AESD_ASSIGNMENTS_VERSION = 'e04da21a5c69d8ee0a7ad5423158901ff332e88c'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -18,14 +18,32 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
 
-# TODO (WIP) add your writer, finder and finder-test utilities/scripts to the installation steps below
+# Content of finder-app/ directory
+#
+# finder-app/start-qemu-app.sh
+# finder-app/dependencies.sh
+# finder-app/writer
+# finder-app/autorun-qemu.sh
+# finder-app/finder.sh
+# finder-app/start-qemu-terminal.sh
+# finder-app/manual-linux.sh
+# finder-app/Makefile
+# finder-app/writer.sh
+# finder-app/writer.c
+# finder-app/finder-test.sh
+# finder-app/conf
+
+
+# TODO (DONE) add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	echo '*******************************'
-	echo 'DEBUG:' "$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin"
-	echo '*******************************'
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
